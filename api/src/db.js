b9669f5e366a07643a7aa276acc3094457857e7e
Liam <iliamftw2013@gmail.com>
2024-06-20 15:56:34 -0300

    Add( item table )

--- api/src/db.js
+++ api/src/db.js
@@ -6,6 +6,20 @@ export default class Database {
 
 createTable(){
     this.db.serialize(() = {
-    this.db.run("CREATE TABLE IF NOT EXISTS _ (_, _, _, _,)");
+    this.db.run("CREATE TABLE IF NOT EXISTS _ (_, _, _, _,)")
     })
+  }
+
+
+insertData(id, title, tweet) {
+    this.db.run(
+      "INSERT INTO ___(_,_,_) VALUES (?, ?, ?)",
+      [],
+      (err) => {
+        if (err) {
+          return console.log(err.message);
+        }
+        console.log("Row was added to the table");
+      },
+    );
   }
