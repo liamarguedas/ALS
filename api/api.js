563d050044d5763d0a7f30195aaa44f71f15df5c
Liam <iliamftw2013@gmail.com>
2024-06-25 17:05:38 -0300

    Add( api structure ready to use )

--- api/api.js
+++ api/api.js
@@ -1,1 +1,17 @@
 // MAIN API ENTRY POINT
+//
+//
+
+import express from "express";
+import bodyParser from "body-parser";
+
+const api = express();
+const port = 3000; // change in client !!!!
+
+api.get("/", (req, res) => {
+  res.send("HELLO WORLD");
+});
+
+api.listen(port, () => {
+  console.log("FUNCIONANDOOOOOO");
+});
