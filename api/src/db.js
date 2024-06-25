0b8f241792371a4e844c1d8b2f2a206f24c00836
Liam <iliamftw2013@gmail.com>
2024-06-25 16:59:41 -0300

    Add( db class ready to use )

--- api/src/db.js
+++ api/src/db.js
@@ -1,4 +1,7 @@
 // POSTGREE DB
+
+import pg from "pg";
+
 export default class Database {
   consturctor(db) {
     this.db = connectDB(db);
@@ -14,7 +17,64 @@ export default class Database {
 
       this.db.run(query, queryValues);
     });
-    return;
+  }
+
+  insertTable(tableName, values, filter = false, filterCondition = false) {
+    this.db.serialize(() => {
+      if (filter) {
+        const query = "INSERT INTO $1($2) $3";
+        const queryValues = [tableName, values, filterCondition];
+      } else {
+        const query = "INSERT INTO $1($2)";
+        const queryValues = [tableName, values];
+      }
+
+      this.db.run(query, queryValues);
+
+      // NEEDS TO RETURN SOMETHING, TODO!!
+      //
+      //
+    });
+  }
+
+  selectData(tableName) {
+    this.db.serialize(() => {
+      const result = this.db.run("SELECT * FROM $1", [tableName]);
+      return result;
+    });
+  }
+
+  selectDataColumn(tableName, column) {
+    this.db.serialize(() => {
+      const query = "SELECT $1 FROM $2";
+      const queryValues = [column, tableName];
+
+      const result = this.db.run(query, queryValues);
+      return result;
+    });
+  }
+
+  filterRow(tableName, column, value) {
+    this.db.serialize(() => {
+      const query = "SELECT $1 FROM $2 WHERE $3 = $4";
+      const queryValues = [column, tableName, column, value];
+
+      const result = this.db.run(query, queryValues);
+      return result;
+    });
+  }
+
+  deleteDataFilter(tableName, column, value) {
+    this.db.serialize(() => {
+      const query = "DELETE FROM $1 WHERE $2 = $3";
+      const queryValues = [tableName, column, value];
+
+      this.db.run(query, queryValues);
+    });
+  }
+
+  closeDb() {
+    this.db.close();
   }
 }
 
@@ -22,15 +82,36 @@ export default class Database {
 function connectDB(db) {
   switch (db) {
     case "sourcebans":
-      return; //todo
+      const dbConection = new pg.Client({
+        user: "postgress",
+        host: "localhost",
+        database: "sourcebans",
+        password: "NAO SEI", // todo
+        port: "NAO SEI", // TODO
+      });
+      return dbConection; //todo
 
     case "hlstats":
-      return; //todo
+      const dbConection = new pg.Client({
+        user: "postgress",
+        host: "localhost",
+        database: "hlstats",
+        password: "NAO SEI", // todo
+        port: "NAO SEI", // TODO
+      });
+      return dbConection; //todo
 
     case "web":
-      return;
+      const dbConection = new pg.Client({
+        user: "postgress",
+        host: "localhost",
+        database: "web",
+        password: "NAO SEI", // todo
+        port: "NAO SEI", // TODO
+      });
+      return dbConection; //todo
 
     default:
-      return; // returns null value itself
+      return new Array(); // returns empty array in case of no db provided
   }
 }
